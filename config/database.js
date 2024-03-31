bffdb88ac40f9aad180c5884708b06f2a5cc5493
Jawad Ali <jawadal829@gmail.com>
2024-03-31 13:08:01 +0500

    project : junk files deleted

--- config/database.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-const connect_database = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(console.log())
-}
--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-require("dotenv").config({ path: "./config/.env" });
-const app = express();
-app.listen(process.env.PORT || 8080, () => {
-  console.log("server is on");
-});
